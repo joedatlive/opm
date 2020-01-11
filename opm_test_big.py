0fe6e1d426757854bbdd3dffebc032d2f2cb2ed8
joedatlive <joed@live.com>
2020-01-11 15:47:03 -0800

    Fixing date types in test files

--- opm_test_big.py
+++ opm_test_big.py
@@ -2,9 +2,13 @@ import urllib.request
 import json
 from opm import option_price_model
 from opm_price_tests import price_test
+from datetime import datetime, date
 
 class Option(object):
     pass
+
+def get_date(timestamp):
+    return(date.fromtimestamp(timestamp))
 
 def get_option_data(puts_or_calls):
     # pull down json with option data from yahoo finance
@@ -23,14 +27,16 @@ def get_option_data(puts_or_calls):
             o.option_type = "put"
         o.strike_price = option_list[i]["strike"]
         o.stock_price = option_list[i]["lastPrice"]
-        o.days_to_expiration = option_list[i]["expiration"]
+        o.expiration = get_date(option_list[i]["expiration"])
+        o.days_to_expiration = (o.expiration - date.today()).days
         o.implied_volatility = option_list[i]["impliedVolatility"]
         o.risk_free_interest_rate = option_list[i]["openInterest"]
         o.bid = option_list[i]["bid"]
         o.ask = option_list[i]["ask"]
         o.contract_symbol = option_list[i]["contractSymbol"]
-        o.last_trade_date = option_list[i]["lastTradeDate"]
+        o.last_trade_date = get_date(option_list[i]["lastTradeDate"])
         price = option_price_model(o.option_place, o.option_type, o.strike_price, o.stock_price, o.days_to_expiration, o.implied_volatility, o.risk_free_interest_rate)
+        print("expiration:", o.expiration)
         print("days to exp:", o.days_to_expiration)
         print("last trade date:", o.last_trade_date)
         price_test(price, o.bid, o.ask, o.option_type, o.contract_symbol, o.last_trade_date)
--- opm_test_unit.py
+++ opm_test_unit.py
@@ -1,16 +1,19 @@
 #static unit test file for opm.py
 #data from yaho finance https://finance.yahoo.com/quote/msft/options/
-contract_name = "MSFT191220C00070000"	
-last_trade_date = "2019-12-13 2:12PM EST"
+
 from opm import option_price_model
 from opm_price_tests import price_test
+from datetime import date
 
-option_date = 2019-12-13
+contract_name = "MSFT191220C00070000"	
+last_trade_date = "2019-12-13 2:12PM EST"
+
+option_date = date(2019,12,13)
 option_place = "european"
 option_type = "call"
 strike = 70.00
 last_price = 84.55
-expiration = 2019-12-20
+expiration = date(2019,12,20)
 bid = 83.45
 ask = 85.70
 change = 2.00
@@ -19,7 +22,7 @@ volume = 15
 open_interest = 14
 implied_volatility = 2.375
 
-days_to_expiration =  option_date - expiration
+days_to_expiration =  (expiration - option_date).days
 
 #calculate risk free interest rate
 #maybe use libor or for american stocks use -tbill rate for the same term annualized.  For testing we will use 1.92 from here: https://www.macrotrends.net/2515/1-year-libor-rate-historical-chart
